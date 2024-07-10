8730f774287c8dac4f6dea1b43c53e6fc972587e
Mitchell Grenier <mitchell@confurious.io>
2024-07-09 21:47:41 -0700

    Add new features to generator

--- tools/obsidian-directed-graph-exporter.py
+++ tools/obsidian-directed-graph-exporter.py
@@ -4,7 +4,7 @@ Usage
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python obsidian-directed-graph-exporter.py {{path_to_markdown_files}} {{path_to_output_images}} {{first_slide_name}} {{gen_images true|false}}
+python obsidian-directed-graph-exporter.py {{path_to_markdown_files}} {{entrypoint md}} {{path_to_output_images}} {{gen_images true|false}}
 """
 
 import os
@@ -13,10 +13,13 @@ import json
 import argparse
 from typing import List, Optional, Tuple
 from PIL import Image, ImageDraw, ImageFont
+from pygments import highlight
+from pygments.lexers import RustLexer
+from pygments.formatters import ImageFormatter
 
 FONT_PATH = "./assets/fonts/Open_Sans/OpenSans-Italic-VariableFont_wdth,wght.ttf"
 # FONT_PATH = "./assets/fonts/AppleColorEmoji.ttf"
-SLIDE_BACKGROUND_IMAGE_PATH = "./assets/background_grad.png"
+SLIDE_BACKGROUND_IMAGE_PATH = "./assets/background_lightmode.png"
 CONTAINS_PATH_POLL = "CONTAINS_PATH_POLL"
 STATE_POLL = "STATE_POLL"
 
@@ -67,22 +70,28 @@ class Poll:
             })
         return fields
 
+class SlideImage:
+    def __init__(self, path: str, scale: int):
+        self.path = path
+        self.scale = scale
+
 class Slide:
-    def __init__(self, index: int, title: str, text_contents: Optional[str], image_contents: Optional[str], poll: Poll, save_path: str, next_slide_name: str):
+    def __init__(self, index: int, title: str, text_contents: Optional[str], image: Optional[SlideImage], poll: Poll, save_path: str, next_slide_name: str, emojis: List[str] = []):
         self.index = index
         self.title = title
         self.text_contents = text_contents
-        self.image_contents = image_contents
+        self.image = image
         self.poll = poll
         self.save_path = save_path
         self.next_slide_name = next_slide_name
+        self.emojis = emojis
     
     def export(self, slides_map):
         fields = {
             'index': self.index,
             'message': self.title,
             'slide': self.save_path,
-            "emojis": [],
+            "emojis": self.emojis,
         }
         
         if self.poll is not None:
@@ -150,7 +159,7 @@ def parse_contents_for_poll(text: str) -> Poll:
 
     return Poll(title, poll_type, entries)
 
-def parse_md_file(file_path: str, index: int) -> Slide:
+def parse_md_file(md_dir: str, file_path: str, index: int) -> Slide:
     """
     Parse a markdown file and extract slide and poll data.
 
@@ -164,22 +173,13 @@ def parse_md_file(file_path: str, index: int) -> Slide:
     with open(file_path, 'r') as file:
         content = file.read()
     
+    # Parse out possible emojis
+    emojis_match = re.search(r'^Emojis:\s*(.*)$', content)
+    emojis = emojis_match.group(1).strip().split(",") if emojis_match else []
+    emojis = [e.strip() for e in emojis]
+
     # Parse out possible poll
     maybe_poll = parse_contents_for_poll(content)
-
-    # Parse out possible image contents in format ![[image_path.png]]
-    slide_image_contents_match = re.search(r'\!\[\[(.*?)\]\]', content)
-    slide_image_contents = slide_image_contents_match.group(1).strip() if slide_image_contents_match else None
-    
-    if slide_image_contents_match:
-        print(f"Parsed out image: {slide_image_contents}")
-
-    # Parse out possible text contents tagged with text
-    slide_text_contents_match = re.search(r"^[Tt]ext: (.*)$", content, re.MULTILINE)
-    slide_text_contents = slide_text_contents_match.group(1).strip() if slide_text_contents_match else content
-
-    if slide_text_contents_match:
-        print(f"Parsed out text: {slide_text_contents}")
 
     # Parse which slide comes next (if direct)
     next_slide_name_match = re.search(r'Next Slide:\s*\[\[(.*?)\]\]', content)
@@ -188,11 +188,45 @@ def parse_md_file(file_path: str, index: int) -> Slide:
     # Parse filename to be set as slide message
     message = os.path.basename(file_path).split(".md")[0]
     message_slug = slugify(message)
+
+    # Parse out possible image contents in format ![[image_path.png]]
+    slide_image_name_match = re.search(r'\!\[\[(.*?)\]\]', content)
+    slide_image_name = slide_image_name_match.group(1).strip() if slide_image_name_match else None
+    slide_image = None
+    if slide_image_name_match:
+        slide_image_scale_match = re.search(r'Scale:\s*(.*)', content)
+        scale = slide_image_scale_match.group(1).strip() if slide_image_scale_match else 1
+        slide_image = SlideImage(os.path.join(md_dir, slide_image_name), int(scale))
+        print(f"Parsed out image: {slide_image.path} with scale {slide_image.scale}")
+
+
+    # Parse out possible text contents tagged with text
+    slide_text_contents_match = re.search(r"^[Tt]ext: (.*)$", content, re.MULTILINE)
+    slide_text_contents = slide_text_contents_match.group(1).strip() if slide_text_contents_match else content
+    if slide_text_contents_match:
+        print(f"Parsed out text: {slide_text_contents}")
+
+    # Parse out if the contents of the slide contain Rust code
+    slide_code_contents_match = re.search(r"```rust((.|\n)*)```", content, re.MULTILINE)
+    slide_code_contents = slide_code_contents_match.group(1).strip() if slide_code_contents_match else content
+    if slide_code_contents_match:
+        print(f"Parsed out code: {slide_code_contents}")
+            
+        lexer = RustLexer()
+        formatter = ImageFormatter(font_name='Andale Mono', line_numbers=False, style='monokai', font_size=42)
+        
+        slide_image = SlideImage(f"output_code_images/{index:03d}.{message_slug}_code.png", 1)
+        
+        # Highlight the code and save it to an image
+        with open(slide_image.path, "wb") as f:
+            highlight(slide_code_contents, lexer, formatter, outfile=f)
+        
+        print(f"Wrote out code image: {slide_image.path}")
     
     # Image path to save, referenced in slides.json as well
     save_path = f"{index:03d}.{message_slug}.png"
 
-    return Slide(index, message, slide_text_contents, slide_image_contents, maybe_poll, save_path, next_slide_name)
+    return Slide(index, message, slide_text_contents, slide_image, maybe_poll, save_path, next_slide_name, emojis)
 
 def process_directory(md_dir: str, entry_slide_title: str) -> List[Slide]:
     """
@@ -215,7 +249,7 @@ def process_directory(md_dir: str, entry_slide_title: str) -> List[Slide]:
 
     for index, md_file in enumerate(md_files):
         file_path = os.path.join(md_dir, md_file)
-        slide = parse_md_file(file_path, index)
+        slide = parse_md_file(md_dir, file_path, index)
         slide_data.append(slide)
 
     return slide_data
@@ -229,44 +263,171 @@ def generate_slide_image(slide: Slide, md_dir: str, output_dir: str):
     slide (Slide): An instance of the Slide class.
     output_dir (str): The directory to save the output images.
     """
-    # TODO - this is just naive, draw centered text if slide has text, draw centered image if 
-    # slide has image. Improve layout possibilities
     
-    # TODO support emoji font 
-
     # First draw background image 
     base_img = Image.open(SLIDE_BACKGROUND_IMAGE_PATH).convert("RGBA")
+    
+    # Store the dimensions of the base image so we know how to scale stuff in the future
+    base_img_x = base_img.size[0]
+    base_img_y = base_img.size[1]
 
     # Draw slide image if any
-    if slide.image_contents is not None:
-        slide_image_path = os.path.join(md_dir, slide.image_contents)
-        if not os.path.exists(slide_image_path):
-            print(f"Slide references image but image path could not be loaded! Slide{slide.title} image path {slide.image_contents}")
+    if slide.image is not None:
+        print(f"Drawing slide {slide.title} With Image - {slide.image.path}")
+        if not os.path.exists(slide.image.path):
+            print(f"Slide references image but image path could not be loaded! Slide {slide.title} image path {slide.image.path}")
         
-        print(f"Drawing slide image {slide_image_path} - {slide.image_contents}")
-        slide_image = Image.open(slide_image_path).convert("RGBA")
+        
+        slide_image = Image.open(slide.image.path).convert("RGBA")
+
+        x_scale = slide_image.size[0] / base_img_x
+        y_scale = slide_image.size[1] / base_img_y
+
+        # Scale image to fit within base image
+        if x_scale > 1 or y_scale > 1:
+            scale = max(x_scale, y_scale)
+            slide_image = slide_image.resize((int(slide_image.size[0] / scale), int(slide_image.size[1] / scale)))
+        else:
+            slide_image = slide_image.resize((int(slide_image.size[0] * slide.image.scale), int(slide_image.size[1] * slide.image.scale)))
+
+
         slide_image_width, slide_image_height = slide_image.size
         img_x = (1920 - slide_image_width) // 2
         img_y = (1080 - slide_image_height) // 2
-        base_img.paste(slide_image, (img_x, img_y))
+        base_img.paste(slide_image, (img_x, img_y), slide_image)
 
     # Draw text if any
     elif slide.text_contents is not None:
-        draw = ImageDraw.Draw(base_img)
+        print(f"Drawing slide {slide.title} With Text - {slide.text_contents}")
         font_size = 40
-        font = ImageFont.truetype(FONT_PATH, font_size, encoding='unic')
-        text_width, text_height = draw.textsize(slide.text_contents, font=font)
+        text = slide.text_contents
+
+        font = ImageFont.truetype("./assets/fonts/Export/SFProDisplay/OpenType-TT/SF-Pro-Display-Medium.ttf", 160)
+        emojiFont = ImageFont.truetype("/System/Library/Fonts/Apple Color Emoji.ttc", 160)
+
+        draw = ImageDraw.Draw(base_img)
+        text_width, text_height = draw.textsize(text, font=font)
         text_x = (1920 - text_width) // 2
         text_y = (1080 - text_height) // 2
         
-        text = slide.text_contents
         draw.text((text_x, text_y), text, fill=(105, 105, 105), font=font)  # Dark grey text
+
+        addEmojis(base_img,  text, (text_x, text_y), font, emojiFont, 165)
 
     output_path = os.path.join(output_dir, slide.save_path)
     base_img.save(output_path)
 
 
+def getEmojiMask(font: ImageFont, emoji: str, size: tuple[int, int]) -> Image:
+    """ Makes an image with an emoji using AppleColorEmoji.ttf, this can then be pasted onto the image to show emojis
+    
+    Parameter:
+    (ImageFont)font: The font with the emojis (AppleColorEmoji.ttf); Passed in so font is only loaded once
+    (str)emoji: The unicoded emoji
+    (tuple[int, int])size: The size of the mask
+    
+    Returns:
+    (Image): A transparent image with the emoji
+    
+    """
+
+    mask = Image.new("RGBA", (160, 160), color=(255, 255, 255, 0))
+    draw = ImageDraw.Draw(mask)
+    draw.text((0, 0), emoji, font=font, embedded_color=True)
+    mask = mask.resize(size)
+
+    return mask
+
+def getDimensions(draw: ImageDraw, text: str, font: ImageFont) -> tuple[int, int]:
+    """ Gets the size of text using the font
+    
+    Parameters:
+    (ImageDraw): The draw object of the image
+    (str)text: The text you are getting the size of
+    (ImageFont)font: The font being used in drawing the text
+    
+    Returns:
+    (tuple[int, int]): The width and height of the text
+    
+    """
+    left, top, right, bottom = draw.multiline_textbbox((0, 0), text, font=font)
+    return (right-left), (bottom-top)
+
+def addEmojis():
+    # Now add any emojis that weren't embedded correctly
+    modifiedResponseL = modifiedResponse.split("\n")
+    for i, line in enumerate(modifiedResponseL):
+        for j, char in enumerate(line):
+            if (not char.isascii()):
+                
+                # Get the height of the text ABOVE the emoji in modifiedResponse
+                aboveText = "\n".join(modifiedResponseL[:i])
+                _, aboveTextHeight = getDimensions(draw, aboveText, poppinsFont)
+
+                # The height that we paste at is aboveTextHeight + (marginHeight+PADDING) + (Some error)
+                # (marginHeight+PADDING) is where we pasted the entire paragraph
+                y = aboveTextHeight + (marginHeight+PADDING) + 5
+
+                # Get the length of the text on the line up to the emoji
+                beforeLength, _ = getDimensions(draw, line[:j], poppinsFont)
+
+                # The x position is beforeLength + 75; 75px is where we pasted the entire paragraph
+                x = (75) + beforeLength
+
+                # Create the mask
+                emojiMask = getEmojiMask(emojiFont, char, (165, 165))
+
+                # Paste the mask onto the image
+                img.paste(emojiMask, (int(x), int(y)), emojiMask)
+
+def addEmojis(img: Image, text: str, box: tuple[int, int], font: ImageFont, emojiFont: ImageFont, size: int) -> None:
+    """ Adds emojis to the text
+    
+    Parameters:
+    (Image)img: The image to paste the emojis onto
+    (tuple[int, int])box: The (x,y) pair where the textbox is placed
+    (ImageFont)font: The font of the text
+    (ImageFont)emojiFont: The emoji's font
+    
+    """
+    draw = ImageDraw.Draw(img)
+    width, height = box
+    # Now add any emojis that weren't embedded correctly
+    text_lines = text.split("\n")
+    for i, line in enumerate(text_lines):
+        for j, char in enumerate(line):
+            if (not char.isascii()):
+                
+                # Get the height of the text ABOVE the emoji in modifiedResponse
+                aboveText = "\n".join(text_lines[:i])
+                _, aboveTextHeight = getDimensions(draw, aboveText, font)
+
+                # The height that we paste at is aboveTextHeight + height + (Some error)
+                y = aboveTextHeight + height + 5
+
+                # Get the length of the text on the line up to the emoji
+                beforeLength, _ = getDimensions(draw, line[:j], font)
+
+                # The x position is beforeLength + width
+                x = width + beforeLength
+
+                # Create the mask; You might want to adjust the size parameter
+                emojiMask = getEmojiMask(emojiFont, char, (size, size))
+
+                # Paste the mask onto the image
+                img.paste(emojiMask, (int(x), int(y)), emojiMask)
+
+
 def main():
+    # for i in range(0, 1000):
+    #     try:
+    #         font = ImageFont.truetype("/System/Library/Fonts/Apple Color Emoji.ttc", size=i)
+    #         print(f"Font size {i} loaded")
+    #     except:
+    #         pass
+    
+    # exit(0)
+
     parser = argparse.ArgumentParser(description="Process markdown files into slides data and generate images")
     parser.add_argument('md_dir', type=str, help="The directory containing markdown files")
     parser.add_argument('entry_md', type=str, help="The starting slide")
